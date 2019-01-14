391abe1fd5b23404b33f9e793b7adc4c2eae1e4d
Anthony Thompson <34thompsonas@cua.edu>
2019-01-14 12:10:29 -0500

    MVC version seems to be working

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:MyNewPass@localhost:8889/blogz'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key= 'test123'
